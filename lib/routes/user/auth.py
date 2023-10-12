44534a754ba23a14a1bcedcb939ef5a68f68b13a
windownik <windownik@gmail.com>
2023-10-12 16:44:25 +0300

    rework login

--- lib/routes/user/auth.py
+++ lib/routes/user/auth.py
@@ -23,22 +23,9 @@ auth_url = f"http://{ip_auth_server}:{ip_auth_port}"
 
 
 @app.post(path='/login', tags=['Auth'], responses=get_login_res)
-async def login_user(phone: int, sms_code: int, device_id: str, device_name: str, db=Depends(data_b.connection)):
+async def login_user(user_id: int, db=Depends(data_b.connection)):
     """Login user in service by phone number, device_id and device_name"""
     """Create new account in service with phone number, device_id and device_name"""
-    params = {
-        "phone": phone,
-        "sms_code": sms_code,
-        "device_id": device_id,
-        "device_name": device_name,
-    }
-    res = requests.post(f'{auth_url}/check_sms', params=params)
-    status_code = res.status_code
-    if status_code == 200:
-        user_id = res.json()['user_id']
-    else:
-        return JSONResponse(content=res.json(),
-                            status_code=status_code)
 
     user_data = await conn.read_data(db=db, table='users', id_name='user_id', id_data=user_id)
     if not user_data:
@@ -49,8 +36,6 @@ async def login_user(phone: int, sms_code: int, device_id: str, device_name: str
     user: User = User.parse_obj(user_data[0])
     return JSONResponse(content={"ok": True,
                                  'user': user.dict(),
-                                 'access_token': res.json()['access_token'],
-                                 'refresh_token': res.json()['refresh_token']
                                  },
                         status_code=_status.HTTP_200_OK)
 
