e40bd6fd001195c321e84c7dc058d4babd9f3c93
windownik <windownik@gmail.com>
2023-10-20 17:32:07 +0300

    added user_status

--- lib/routes/user/user.py
+++ lib/routes/user/user.py
@@ -75,6 +75,35 @@ async def update_user(access_token: str, name: str, surname: str, email: str, ge
                         status_code=_status.HTTP_200_OK)
 
 
+@app.put(path='/user_status', tags=['User'], responses=get_user_res)
+async def update_user(access_token: str, db=Depends(data_b.connection)):
+    """Update user information"""
+    res = requests.get(f'{auth_url}/user_id', params={"access_token": access_token})
+    status_code = res.status_code
+    if status_code == 200:
+        user_id = res.json()['user_id']
+    else:
+        return JSONResponse(content=res.json(),
+                            status_code=status_code)
+
+    user_data = await conn.read_data(db=db, table='users', id_name='user_id', id_data=user_id)
+
+    if not user_data:
+        return JSONResponse(content={"ok": False,
+                                     'description': "Error with login account",
+                                     },
+                            status_code=500)
+
+    await conn.update_inform(db=db, name="status", table="users", data="active", id_name="user_id", id_data=user_id)
+    user_data = await conn.read_data(db=db, table='users', id_name='user_id', id_data=user_id)
+
+    user: User = User.parse_obj(user_data[0])
+    return JSONResponse(content={"ok": True,
+                                 'user': user.dict(),
+                                 },
+                        status_code=_status.HTTP_200_OK)
+
+
 @app.delete(path='/user', tags=['User'], responses=delete_user_res)
 async def login_user(access_token: str, db=Depends(data_b.connection)):
     """Create vehicle in service by information"""
