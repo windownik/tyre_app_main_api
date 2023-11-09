4b2f5d910a1434697484ea8a07ff0e4612c83ec5
windownik <windownik@gmail.com>
2023-11-09 13:26:02 +0300

    added get admin_users

--- lib/routes/user/admin_users.py
+++ lib/routes/user/admin_users.py
@@ -69,7 +69,8 @@ async def admin_get_users(access_token: str, search: str = 0, page: int = 0, db=
 
     return JSONResponse(content={"ok": True,
                                  'list_users': list_user,
-                                 "pages": len(new_user_list) // users_on_page + 1
+                                 "pages": len(new_user_list) // users_on_page + 1,
+                                 "users_count": len(user_data)
                                  },
                         status_code=_status.HTTP_200_OK,
                         headers={'content-type': 'application/json; charset=utf-8'})
