bc15152c17682b0ca4d2b3d923c7cb77e650e4e1
windownik <windownik@gmail.com>
2023-11-17 10:56:14 +0300

    Create get me pro route

--- lib/__init__.py
+++ lib/__init__.py
@@ -4,3 +4,4 @@ from lib.routes.vehicle import vehicle
 from lib.routes import system
 from lib.routes.push import push
 from lib.routes.service_sessions import service_session, review, work_types, payments
+from lib.routes.pro import auth
---
+++ lib/routes/pro/auth.py
@@ -0,0 +1,63 @@
+import os
+
+import requests
+import starlette.status as _status
+from fastapi import Depends
+from starlette.responses import JSONResponse
+
+from lib import sql_connect as conn
+from lib.db_objects import User, Vehicle, ServiceSession
+from lib.response_examples import *
+from lib.sql_create_tables import data_b, app
+
+
+ip_server = os.environ.get("IP_SERVER")
+ip_port = os.environ.get("PORT_SERVER")
+
+ip_port = 80 if ip_port is None else ip_port
+ip_server = "127.0.0.1" if ip_server is None else ip_server
+
+ip_auth_server = os.environ.get("IP_AUTH_SERVER")
+ip_auth_port = os.environ.get("PORT_AUTH_SERVER")
+
+auth_url = f"http://{ip_auth_server}:{ip_auth_port}"
+
+
+@app.get(path='/get_me_pro', tags=['Pro'], responses=get_login_res)
+async def login_user(access_token: str, db=Depends(data_b.connection)):
+    """Get user in service by access token"""
+    res = requests.get(f'{auth_url}/user_id', params={"access_token": access_token})
+    status_code = res.status_code
+    if status_code == 200:
+        user_id = res.json()['user_id']
+    else:
+        return JSONResponse(content=res.json(),
+                            status_code=status_code)
+    user_data = await conn.read_data(db=db, table='users', id_name='user_id', id_data=user_id)
+    if not user_data:
+        return JSONResponse(content={"ok": False,
+                                     'description': "Error with login account",
+                                     },
+                            status_code=500)
+    user: User = User.parse_obj(user_data[0])
+
+    # vehicle_data = await conn.read_vehicles(db=db, user_id=user_id)
+
+    # vehicles = []
+    # for one in vehicle_data:
+    #     vehicle: Vehicle = Vehicle.parse_obj(one)
+    #     vehicles.append(vehicle.dict())
+    #
+    # session_data = await conn.read_service_session(db=db, client_id=user_id)
+    # services_sessions = []
+    # for one in session_data:
+    #     session: ServiceSession = ServiceSession.parse_obj(one)
+    #     services_sessions.append(await session.to_json(db=db, session_work_list=[]))
+
+    return JSONResponse(content={"ok": True,
+                                 'user': user.dict(),
+                                 # "vehicles": vehicles,
+                                 # "service_sessions": services_sessions
+                                 },
+                        status_code=_status.HTTP_200_OK,
+                        headers={'content-type': 'application/json; charset=utf-8'})
