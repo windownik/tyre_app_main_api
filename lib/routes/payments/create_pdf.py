9ad0ff6267e40d14f3e89951247bbb818ed80158
windownik <windownik@gmail.com>
2023-12-17 00:04:48 +0300

    work with pdf

--- lib/routes/payments/create_pdf.py
+++ lib/routes/payments/create_pdf.py
@@ -22,8 +22,8 @@ def create_file_pdf(data: tuple):
         pdf = write_headers(pdf)
         pdf = write_body(pdf=pdf, data=payment)
         pdf.cell(100, 8, ln=True)
-        pdf.cell(1000, 2, border=1, ln=True)
-        pdf.cell(100, 12, ln=True)
+        pdf.cell(1000, 0.5, border=1, ln=True)
+        pdf.cell(100, 5, ln=True)
 
     pdf.output("invoice.pdf")
 
@@ -31,7 +31,7 @@ def create_file_pdf(data: tuple):
 def write_for_one_user(pdf: FPDF, name: str) -> FPDF:
     pdf.set_font("Montserrat", "", 18)
     pdf.cell(120, 20, f"Driver's name:  {name}", ln=True)
-    pdf.cell(1000, 2, border=1, )
+    pdf.cell(1000, 0.5, border=1, )
     return pdf
 
 
@@ -57,20 +57,20 @@ def write_headers(pdf: FPDF) -> FPDF:
     pdf.set_font("Montserrat", "", 12)
     pdf.cell(150, 8, "Works in service session", align=Align.L, ln=True)
     pdf.set_font("lite", "", 10)
-    pdf.cell(10, 8, "ID", border=1, align=Align.C)
-    pdf.cell(100, 8, "Work description", border=1, align=Align.C)
-    pdf.cell(20, 8, "Currency", border=1, align=Align.C)
-    pdf.cell(20, 8, "Amount", border=1, align=Align.C)
-    pdf.cell(30, 8, "Pay date", border=1, align=Align.C, ln=True)
+    pdf.cell(10, 6, "ID", border=1, align=Align.C)
+    pdf.cell(100, 6, "Work description", border=1, align=Align.C)
+    pdf.cell(20, 6, "Currency", border=1, align=Align.C)
+    pdf.cell(20, 6, "Amount", border=1, align=Align.C)
+    pdf.cell(45, 6, "Pay date", border=1, align=Align.C, ln=True)
     return pdf
 
 
 def write_body(pdf: FPDF, data: dict) -> FPDF:
     dtime = datetime.datetime.utcfromtimestamp(data["pay_date"])
     pdf.set_font("lite", "", 10)
-    pdf.cell(10, 8, str(data["pay_id"]), border=1, align=Align.C)
-    pdf.cell(100, 8, "Work description", border=1, align=Align.C)
-    pdf.cell(20, 8, data["currency"], border=1, align=Align.C)
-    pdf.cell(20, 8, str(data["amount"] / 100), border=1, align=Align.C)
-    pdf.cell(30, 8, str(dtime), border=1, align=Align.C, ln=True)
+    pdf.cell(10, 6, str(data["pay_id"]), border=1, align=Align.C)
+    pdf.cell(100, 6, "Work description", border=1, align=Align.C)
+    pdf.cell(20, 6, data["currency"], border=1, align=Align.C)
+    pdf.cell(20, 6, str(data["amount"] / 100), border=1, align=Align.C)
+    pdf.cell(45, 6, str(dtime), border=1, align=Align.C, ln=True)
     return pdf
