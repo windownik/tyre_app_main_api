4e90e5843b8bd8505f34c5dc4a8aa128450bbaa6
windownik <windownik@gmail.com>
2023-12-16 23:48:59 +0300

    work with pdf

--- lib/routes/payments/create_pdf.py
+++ lib/routes/payments/create_pdf.py
@@ -20,26 +20,27 @@ def create_file_pdf(data: tuple):
             pdf = write_session_data(pdf=pdf, data=payment, )
         pdf = write_headers(pdf)
         pdf = write_body(pdf=pdf, data=payment)
+        pdf.cell(1000, 2, border=1, ln=True)
 
     pdf.output("invoice.pdf")
 
 
 def write_for_one_user(pdf: FPDF, name: str) -> FPDF:
     pdf.set_font("Montserrat", "", 18)
-    pdf.cell(100, 20, name, ln=True)
-    pdf.cell(1000, 2, border=1,)
+    pdf.cell(120, 20, f"Driver's name:  {name}", ln=True)
+    pdf.cell(1000, 2, border=1, )
     return pdf
 
 
 def write_session_data(pdf: FPDF, data: dict) -> FPDF:
     pdf.set_font("Montserrat", "", 14)
-    pdf.cell(150, 10, f"Session ID # {data['session_id']}", ln=True)
-    pdf.cell(150, 10, f"Vehicle reg number:  {data['reg_num']}", ln=True)
-    pdf.cell(150, 10, f"Vehicle:  {data['make']} {data['model']}", ln=True)
-    pdf.cell(150, 10, f"Vehicle year:  {data['year']}", ln=True)
-    pdf.cell(150, 10, f"Tyre size, Front:  {data['front_section_width']}/ {data['front_aspect_ratio']} "
-                      f"R{data['front_rim_diameter']},  Rear: {data['rear_section_width']}/ {data['rear_aspect_ratio']} "
-                      f"R{data['rear_rim_diameter']}",
+    pdf.cell(150, 8, f"Session ID # {data['session_id']}", ln=True)
+    pdf.cell(150, 8, f"Vehicle reg number:  {data['reg_num']}", ln=True)
+    pdf.cell(150, 8, f"Vehicle:  {data['make']} {data['model']}", ln=True)
+    pdf.cell(150, 8, f"Vehicle year:  {data['year']}", ln=True)
+    pdf.cell(150, 8, f"Tyre size, Front:  {data['front_section_width']}/ {data['front_aspect_ratio']} "
+                     f"R{data['front_rim_diameter']},  Rear: {data['rear_section_width']}/ {data['rear_aspect_ratio']} "
+                     f"R{data['rear_rim_diameter']}",
              ln=True)
     pdf.cell(150, 10, f"Client:  {data['name']} {data['surname']}, tel. {data['phone']}", ln=True)
     pdf.cell(100, 10, ln=True)
@@ -48,22 +49,24 @@ def write_session_data(pdf: FPDF, data: dict) -> FPDF:
 
 def write_headers(pdf: FPDF) -> FPDF:
     pdf.set_font("Montserrat", "", 12)
-    pdf.cell(15, 10, "ID", border=1, align=Align.C)
-    pdf.cell(100, 10, "Work description", border=1, align=Align.C)
-    pdf.cell(30, 10, "Currency", border=1, align=Align.C)
-    pdf.cell(30, 10, "Amount", border=1, align=Align.C)
-    pdf.cell(30, 10, "Pay date", border=1, align=Align.C, ln=True)
+    pdf.cell(150, 8, "Works in service session", border=1, align=Align.C, ln=True)
+    pdf.cell(10, 8, "ID", border=1, align=Align.C)
+    pdf.cell(100, 8, "Work description", border=1, align=Align.C)
+    pdf.cell(20, 8, "Currency", border=1, align=Align.C)
+    pdf.cell(20, 8, "Amount", border=1, align=Align.C)
+    pdf.cell(30, 8, "Pay date", border=1, align=Align.C, ln=True)
     return pdf
 
 
 def write_body(pdf: FPDF, data: dict) -> FPDF:
-
+    dtime = datetime.datetime.utcfromtimestamp(data["pay_date"])
     pdf.set_font("Montserrat", "", 12)
-    pdf.cell(15, 10, str(data["pay_id"]), border=1, align=Align.C)
-    pdf.cell(100, 10, "Work description", border=1, align=Align.C)
-    pdf.cell(30, 10, data["currency"], border=1, align=Align.C)
-    pdf.cell(30, 10, str(data["amount"]/100), border=1, align=Align.C)
-    pdf.cell(30, 10, str(data["pay_date"]), border=1, align=Align.C, ln=True)
+    pdf.cell(10, 8, str(data["pay_id"]), border=1, align=Align.C)
+    pdf.cell(100, 8, "Work description", border=1, align=Align.C)
+    pdf.cell(30, 8, data["currency"], border=1, align=Align.C)
+    pdf.cell(30, 8, str(data["amount"] / 100), border=1, align=Align.C)
+    pdf.cell(30, 8, str(dtime), border=1, align=Align.C, ln=True)
     return pdf
 
 
+print(ti)
